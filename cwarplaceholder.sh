fdd053a4217d6810df7a4a39d61ba5f94a3a8bb2
Jon Elliott <35746415+jelliott27@users.noreply.github.com>
2022-05-22 13:37:16 -0400

    Update cwarplaceholder.sh

--- cwarplaceholder.sh
+++ cwarplaceholder.sh
@@ -10,4 +10,7 @@ git clone https://github.com/jpartemis/cwarp.git
 cd cwarp/
 pip install -r requirements.txt
 pip install streamlit
+pip install markupsafe==2.0.1
+pip3 install Jinja2==3.0
+grep -v "caching.clear_cache()" cwarp_app.py > tmpfile && mv tmpfile cwarp_app.py
 streamlit run cwarp_app.py
